53d181c0824a6405e373005fd95d0259eb12135b
Patrice Wietmaier <64105744+pwiet01@users.noreply.github.com>
2023-08-21 01:14:54 +0200

    Add DaisyUi and more pages (#1)

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,20 +1,31 @@
-<footer class="py-6 flex flex-row justify-between items-center">
-  <div />
+<script>
+  import SvelteLogo from './SvelteLogo.svelte';
+</script>
 
-  <div class="flex flex-row items-center">
-    <div class="opacity-50 mr-3 flex items-center">
-      <i class="fa-regular fa-copyright fa-2xs mr-1.5" />
-      Patrice Wietmaier
-    </div>
+<footer class="navbar px-0 justify-between">
+  <a
+    href="https://kit.svelte.dev/"
+    target="_blank"
+    class="pl-0 flex-1 hidden sm:flex flex-row items-center"
+  >
+    Made with
+    <SvelteLogo class="w-auto h-7 ml-1" />
+  </a>
+
+  <div class="flex-1 sm:flex-none flex flex-row items-center justify-end">
+    <a href="/imprint"> Impressum </a>
+
+    <div class="divider divider-horizontal" />
 
     <a
       href="https://www.linkedin.com/in/patrice-wietmaier"
+      target="_blank"
       class="hover:text-blue-500 focus:text-blue-500"
     >
       <i class="fa-brands fa-linkedin fa-xl" />
     </a>
 
-    <a href="https://github.com/pwiet01">
+    <a href="https://github.com/pwiet01" target="_blank" class="pr-0">
       <i class="fa-brands fa-github fa-xl" />
     </a>
   </div>
---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,22 @@
+<div class="w-full h-full flex flex-col justify-center">
+  <div class="flex justify-center">
+    <div class="flex-1 card bg-base-200 shadow-xl max-w-3xl">
+      <div class="card-body">
+        <h1 class="mb-6">Impressum</h1>
+
+        <div class="mb-4">
+          <h3 class="mb-2 opacity-50">Betreiber</h3>
+          <p>Patrice Wietmaier</p>
+          <p>Luisenstr. 32</p>
+          <p>49074 Osnabrück</p>
+          <p>Deutschland</p>
+        </div>
+
+        <div>
+          <h3 class="mb-2 opacity-50">Kontakt</h3>
+          <p>mail@pwietmaier.de</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
